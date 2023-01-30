b1d623480851222423ffaa55c47e77cbab53ba4d
Sarvesh Bachute <sarveshbachute@gmail.com>
2023-01-31 02:18:25 +0530

    final commit2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { TextForm } from "./components/TextForm";
 import { About } from "./components/About";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Link
